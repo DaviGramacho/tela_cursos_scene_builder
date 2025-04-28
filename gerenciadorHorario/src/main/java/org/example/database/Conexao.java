ddc6260a35ec3331509765ee1a4ba3cb4eb80ba2
Davi Gramacho <piresdavi135@gmail.com>
2025-04-27 21:57:25 -0300

    adicionando alguns arquivos de bd

---
+++ gerenciadorHorario/src/main/java/org/example/database/Conexao.java
@@ -0,0 +1,29 @@
+package org.example.database;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+
+    static String url = "jdbc:mysql://localhost:3306/bancoapi2";
+    static String user = "root";
+    static String password = "040602";
+
+    public static Connection conectar() {
+        try {
+            return DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            System.err.println("Erro ao conectar ao banco de dados: " + e.getMessage());
+            return null;
+        }
+    }
+}
+
+
+
+
+
+
+
